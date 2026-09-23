a44bb5e240ddd55bca7cd66120e70823eacb6bd6
lechefescargot <chrislainassi@outlook.fr>
2026-09-23 21:54:58 +0000

    Update config.js

--- deuxieme-vie 2/config.js
+++ deuxieme-vie 2/config.js
@@ -5,5 +5,5 @@
 // la sécurité est assurée par les règles RLS de supabase/schema.sql.
 // Ne mettez JAMAIS la clé "service_role" ici.
 
-export const SUPABASE_URL = "https://gqxlaxjgdfmvzdlhexal.supabase.co";
+export const SUPABASE_URL = "https://gqxlaxjgdfmvzdlhexal.supabase.co/rest/v1/";
 export const SUPABASE_ANON_KEY = "sb_publishable_z6xgV5AJRelG-Gk7rhYEKg_ZewCUbFI";
